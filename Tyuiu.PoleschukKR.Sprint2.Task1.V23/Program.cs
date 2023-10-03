6e84544dd0426bc9e04b18ff370ea2b32725cd8f
Tyuiu.PoleschukKR@outlook.com <kirillpolesuk05@gmail.com>
2023-10-03 21:28:21 +0500

    Task1

---
+++ Tyuiu.PoleschukKR.Sprint2.Task1.V23/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PoleschukKR.Sprint2.Task1.V23.Lib;
+
+
+namespace Tyuiu.PoleschukKR.Sprint2.Task1.V23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Полещук К.Р. | ИИПб-23-1";
+            //Длина строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы разветвляющей структуры. Логические операции            *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #23                                                             *");
+            Console.WriteLine("* Выполнил: Полещук Кирилл Романович | ИИПб-23-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=,         *");
+            Console.WriteLine("*последовательность можно чередовать, но использовать один раз в выражении*");
+            Console.WriteLine("* и логических операций (|, &, ||, &&, !, ^, последовательность операций  *");
+            Console.WriteLine("* не должна нарушаться), а также арифметических выражений, которая вернет *");
+            Console.WriteLine("* логическую последовательность(массив): (False, False, False, True, True,*");
+            Console.WriteLine("*  True) при a = 242, b = 571, c = 325, d = 155                           *");
+            Console.WriteLine("*                                                                         *");
+
+            int a = 242;
+            int b = 571;
+            int c = 325;
+            int d = 155;
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+            Console.WriteLine("c = " + c);
+            Console.WriteLine("d = " + d);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
