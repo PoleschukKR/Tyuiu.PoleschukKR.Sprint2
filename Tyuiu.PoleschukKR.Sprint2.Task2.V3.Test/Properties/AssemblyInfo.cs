39502ab35e094013e507434094cf32518ec7ccf9
Tyuiu.PoleschukKR@outlook.com <kirillpolesuk05@gmail.com>
2023-10-05 15:15:25 +0500

    Task2

---
+++ Tyuiu.PoleschukKR.Sprint2.Task2.V3.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PoleschukKR.Sprint2.Task2.V3.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PoleschukKR.Sprint2.Task2.V3.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("74606d8e-f3ae-4866-8fde-3cae6bb92c2f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.PoleschukKR.Sprint2.Task2.V3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PoleschukKR.Sprint2.Task2.V3.Lib;
+
+namespace Tyuiu.PoleschukKR.Sprint2.Task2.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Полещук К.Р. | ИИПб-23-1";
+            //Длина строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы разветвляющей структуры. Оператор if                    *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнил: Полещук Кирилл Романович | ИИПб-23-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("*Написать программу на, которая запрашивает целые значения с клавиатуры и *");
+            Console.WriteLine("*вычисляет находится ли точка с координатами X,Y в заштрихованной области *");
+            Console.WriteLine("*                                                                         *");
+
+            int x;
+            int y;
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(" Введите значение переменной х: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" Введите значение переменной y: ");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            if (res == true)
+                Console.WriteLine("Точка с данными координатами является заштрихованной");
+            else
+                Console.WriteLine("Точка с данными координатами не является заштрихованной");
+            Console.ReadKey();
+        }
+    }
+}
