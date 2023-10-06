f5d06777d4e888b883f48a54e05dd3a7d1e22429
Tyuiu.PoleschukKR@outlook.com <kirillpolesuk05@gmail.com>
2023-10-06 15:20:00 +0500

    Task4

---
+++ Tyuiu.PoleschukKR.Sprint2.Task4.V18.Test/DataServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.PoleschukKR.Sprint2.Task4.V18.Lib;
+
+namespace Tyuiu.PoleschukKR.Sprint2.Task4.V18.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 5;
+            double y = 1;
+            double res = ds.Calculate(x, y);
+            double wait = 14.8;
+            Assert.AreEqual(wait, res);
+
+        }
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 10;
+            double res = ds.Calculate(x, y);
+            double wait = 36.012;
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
---
+++ Tyuiu.PoleschukKR.Sprint2.Task4.V18/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PoleschukKR.Sprint2.Task4.V18.Lib;
+
+namespace Tyuiu.PoleschukKR.Sprint2.Task4.V18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Полещук К.Р. | ИИПб-23-1";
+            //Длина строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы разветвляющей структуры. Тернарный оператор             *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #18                                                             *");
+            Console.WriteLine("* Выполнил: Полещук Кирилл Романович | ИИПб-23-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение с              *");
+            Console.WriteLine("* использованием тернарного оператора, где пользователь вводит значение   *");
+            Console.WriteLine("* переменных x,y с клавиатуры                                             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(" Введите значение переменной х: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+           
+
+            Console.WriteLine(" Введите значение переменной y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            double res = ds.Calculate(x,y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Значение функции = " + res);
+            Console.ReadKey();
+
+
+        }
+    }
+}
